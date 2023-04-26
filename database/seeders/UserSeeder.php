cc9c71237a31f1044fdce39d414b6183f00ab95e
pravesh-ve <praveshyadav@virtualemployee.com>
2023-04-26 13:45:51 +0530

    added is_admin key in user table and create seeder for admin

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'email'=>'admin@radiologycheck.com',
+            'password' => Hash::make('admin@123'),
+            'first_name' => 'admin',
+            'last_name' => 'radiology',
+            'is_admin' => '0'
+        ]);
+    }
+}
