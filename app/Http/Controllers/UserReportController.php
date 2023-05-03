10431c29589309e2c84d701f8a9105d0adb3009c
pravesh-ve <praveshyadav@virtualemployee.com>
2023-05-03 18:47:03 +0530

    create report list api

--- app/Http/Controllers/UserReportController.php
+++ app/Http/Controllers/UserReportController.php
@@ -10,4 +10,10 @@ class UserReportController extends Controller
     public function storeUserReport(Request $request, UserReport $userReport){
         return $userReport->saveUserReport($request);
     }
+
+    public function getUserReport(UserReport $userReport,$user_id)
+    {
+        return $userReport->getUserReport($user_id);
+        
+    }
 }
